cef4ad7650228a182380b52f730f3c27dd64f680
P-Trofimov <trofimovpavelv@gmail.com>
2019-04-13 16:22:58 +0300

    Updates

---
+++ precache-manifest.985c839f8dc13cb1bf346e3672ab9927.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a77af33a5586b22f463785f4082d053",
+    "url": "/combat-description-cards/static/media/finesse.6a77af33.svg"
+  },
+  {
+    "revision": "aa5c411fb2bc922b7fcb",
+    "url": "/combat-description-cards/static/css/main.6d2d9e68.chunk.css"
+  },
+  {
+    "revision": "27c816532b16c57de374",
+    "url": "/combat-description-cards/static/js/runtime~main.f8dae531.js"
+  },
+  {
+    "revision": "e1d5036797a259812ce8",
+    "url": "/combat-description-cards/static/js/2.df838ba6.chunk.js"
+  },
+  {
+    "revision": "f584a2b0002a98fd4ceb04303262c7d0",
+    "url": "/combat-description-cards/static/media/fire2.f584a2b0.jpg"
+  },
+  {
+    "revision": "cde61948061b41f5de5c9d04d0ade8be",
+    "url": "/combat-description-cards/static/media/sword.cde61948.svg"
+  },
+  {
+    "revision": "aa5c411fb2bc922b7fcb",
+    "url": "/combat-description-cards/static/js/main.84cfb2bc.chunk.js"
+  },
+  {
+    "revision": "aa8d171faee727ec8e70590d05443612",
+    "url": "/combat-description-cards/static/media/spear.aa8d171f.svg"
+  },
+  {
+    "revision": "7ab3977b2a1b02f23bed7269f3dd749a",
+    "url": "/combat-description-cards/static/media/hammer.7ab3977b.svg"
+  },
+  {
+    "revision": "89470ca1c5fbee0565529ad4e25c6815",
+    "url": "/combat-description-cards/static/media/power.89470ca1.svg"
+  },
+  {
+    "revision": "caf7e90986828f60d3c21f90445d8c61",
+    "url": "/combat-description-cards/static/media/bow.caf7e909.svg"
+  },
+  {
+    "revision": "bd3f52bbbc81ad283074432905b2ef79",
+    "url": "/combat-description-cards/index.html"
+  }
+];
